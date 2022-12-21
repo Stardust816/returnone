0b1a6eb618616198d08b1037d9b341f44a24a42d
stephanweidinger <ic21b064@technikum-wien.at>
2022-12-21 18:17:07 +0100

    Initial commit

---
+++ src/main/java/com/example/returnone/ReturnoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.returnone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReturnoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReturnoneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/returnone/ReturnoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.returnone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReturnoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
